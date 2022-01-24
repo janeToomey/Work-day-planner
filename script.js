655a431f869c355fc6fdead779018601be3766a0
janeToomey <j.toomey1027@gmail.com>
2022-01-23 16:49:59 -0800

    added the link to my js file and added some js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 //render current date- d/m/y
-$('#currentDate').text(moment().format('dddd, MMMM Do YYYY, h:mm'));
+$('#currentDay').text(moment().format('dddd, MMMM Do'));
 
+//makes function available after doc loads, uses jquery
+$(document).ready(function(){
+    $('.saveBtn').on('click', function(){
+        var type = $(this). siblings('.description').val();
+
+        //saves user input to local storage
+        localStorage.setItem(time, text);
+    })
+
+    function colorChange(){
+        var currentTime = moment().hours();
+
+        $('.conatiner-row').each(function(){
+            var scheduleHour = pareInt ($(this).attr('id'));
+
+            if (scheduleHour <currentTime){
+                $(this).addClass('past');
+            } else if (scheduleHour === currentTime){
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).removeClass('future');
+            }
+        })
+    }
+    colorChange();
+
+});
